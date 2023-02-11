e4be80c2a2961f92e1206464804292c751183c7f
Richard <richardzjm@gmail.com>
2023-02-11 12:13:03 -0500

    compliation script for preselected cfgs

---
+++ phase2/activeLearningDFT/isolateNextDFTConfig.py
@@ -0,0 +1,19 @@
+import shutil
+import re
+import os
+import sys
+
+#This script complies all the individual preselected config scripts in the md runs parent folder and complies them all
+#This allows the MLP diff script to prune the similar new configurations an elimiante similar datasets prior to further ab initio calculation
+
+masterConfigFileLocation = "./preselected.cfg"
+mdRunsLocation = "../runs/MDRuns/"
+
+
+
+with open(masterConfigFileLocation,'wb') as master:
+    for childDirectories, descendantDirectories, files in os.walk(masterConfigFileLocation):
+        for runDirectory in childDirectories:
+            childPreselectedConfigName = mdRunsLocation + runDirectory + "preselected.cfg"
+            with open(childPreselectedConfigName,'rb') as child:
+                shutil.copyfileobj(child, master)
