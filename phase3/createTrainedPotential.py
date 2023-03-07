d87d71c5292561a672e09d0dc3712290ba6a1760
Riche <richardzjm@gmail.com>
2023-03-07 15:51:06 -0500

    Added automatic training

--- phase3/createTrainedPotential.py
+++ phase3/createTrainedPotential.py
@@ -42,9 +42,19 @@ try:
 except:
     pass
 
-rootFolder = os.getcwd()    
-# If there are already inital DFT results, simply pass and continue with the learning step
+rootFolder = os.path.dirname(os.path.realpath(__file__))              # Get useful folder locations
 DFToutputFolder = rootFolder + "/outputDFT"
+templatesFolder = rootFolder + "/templates"
+scriptsFolder = rootFolder + "/pythonScripts"   
+mtpFolder = rootFolder + "/mtpProperties"
+os.chdir(rootFolder)
+
+#region Inital Generation
+#=======================================================================
+# Generation of initial DFT results
+#=======================================================================   
+# If there are already inital DFT results, simply pass and continue with the learning step
+
 if not os.path.exists(DFToutputFolder): os.mkdir(DFToutputFolder)
 else: 
     pass
@@ -70,17 +80,13 @@ DFT1AtomStrains = np.arange(params["1AtomDFTStrainRange"][0],params["1AtomDFTStr
 DFT1AtomShears = np.arange(params["1AtomDFTShearRange"][0],params["1AtomDFTShearRange"][1],params["1AtomDFTShearStep"])
 DFT2AtomStrains = np.arange(params["2AtomDFTStrainRange"][0],params["2AtomDFTStrainRange"][1],params["2AtomDFTStrainStep"])
 
-templatesFolder = rootFolder + "/templates"
+
 template1AtomStrainDFT = templatesFolder + "/1AtomStrainDFT.in"
 template1AtomShearDFT = templatesFolder + "/1AtomShearDFT.in"
 template2AtomStrainDFT = templatesFolder + "/2AtomStrainDFT.in"
 templateDFTJob = templatesFolder + "/jobInitialDFT.qsub"
 
-scriptsFolder = rootFolder + "/pythonScripts"               
-followUpScript = scriptsFolder + "/runMDCases.py"
-
 subprocesses = []
-
 
 # Generate and submit the 1Atom Strain runs
 for strain in DFT1AtomStrains:
@@ -116,8 +122,6 @@ for strain in DFT1AtomStrains:
         contentNew = re.sub("\$time", params["dftJobParam"]["time"], contentNew) 
         contentNew = re.sub("\$in", inputName, contentNew)      
         contentNew = re.sub("\$out", outputName, contentNew)
-        contentNew = re.sub("\$next", followUpScript, contentNew)
-        contentNew = re.sub("\$root", rootFolder, contentNew)
         f.seek(0)
         f.write(contentNew)
         f.truncate()
@@ -158,8 +162,6 @@ for shear in DFT1AtomShears:
         contentNew = re.sub("\$time", params["dftJobParam"]["time"], contentNew) 
         contentNew = re.sub("\$in", inputName, contentNew)      
         contentNew = re.sub("\$out", outputName, contentNew)
-        contentNew = re.sub("\$next", followUpScript, contentNew)
-        contentNew = re.sub("\$root", rootFolder, contentNew)
         f.seek(0)
         f.write(contentNew)
         f.truncate()
@@ -199,14 +201,11 @@ for strain in DFT2AtomStrains:
             contentNew = re.sub("\$time", params["dftJobParam"]["time"], contentNew) 
             contentNew = re.sub("\$in", inputName, contentNew)      
             contentNew = re.sub("\$out", outputName, contentNew)
-            contentNew = re.sub("\$next", followUpScript, contentNew)
-            contentNew = re.sub("\$root", rootFolder, contentNew)
             f.seek(0)
             f.write(contentNew)
             f.truncate()
         
         if (not dryRun): subprocesses.append(subprocess.Popen(["sbatch",  jobName]))  
-
 
 exitCodes = [p.wait() for p in subprocesses]        # Wait for all the initial generation to finish
 subprocesses = []
@@ -214,25 +213,77 @@ failure = bool(sum(exitCodes))
 if failure:
     print("One or more of the inital DFT runs has been unsuccessful. Exiting now...")
     quit()
+  
+ #endregion    
+
+#region Active Learning Loop
+#=======================================================================
+# Start of the active learning loop
+#=======================================================================   
+
+# Get some useful file locations for the active learning
+mtpFile = mtpFolder + "/pot.mtp"
+trainingConfigs = mtpFolder + "/train.cfg"
+
+
     
-    
- #-----------------------------------------------------------------------
- # Start of the active learning loop
- #-----------------------------------------------------------------------   
-    
-    
-    
+#-----------------------------------------------------------------------
+# Extraction of results from DFT runs and calculation of mindists
+#-----------------------------------------------------------------------   
 extractionScript = scriptsFolder + "/extractConfigFromDFT.py"
+minddistJobTemplate = templatesFolder + "/runMinDist.qsub"
+minddistJob = DFToutputFolder + "/runMinDist.qsub"
+
+# Extract the outputs from the individual files and assemble a training config file
+# Then, run mindidst on it
 os.chdir(DFToutputFolder)
 exitCode = subprocess.Popen(["python", extractionScript]).wait()
-exitCode = subprocess.Popen([params["mlpBinary"], "mindist", "train.cfg"]).wait()
-os.chdir(rootFolder)
+shutil.copyfile(minddistJobTemplate, minddistJob)
 
-
+# Generate and run mindist job file (job file must be used to avoid clogging login nodes)
+with open (minddistJob, 'r+' ) as f:
+            content = f.read()
+            contentNew = re.sub("\$account", params["slurmParam"]["account"], content) 
+            contentNew = re.sub("\$partition", params["slurmParam"]["partition"], contentNew) 
+            contentNew = re.sub("\$qos", params["slurmParam"]["qos"], contentNew) 
+            contentNew = re.sub("\$mlp", params["mlpBinary"], contentNew)
+            f.seek(0)
+            f.write(contentNew)
+            f.truncate()
+        
+exitCode = subprocess.Popen(["sbatch", minddistJob]).wait()
 if(exitCode):
     print("The mindist call has failed. Potential may be unstable. Exiting...")
     quit()
 
+os.remove(minddistJob)
+# Copy the newly formed training config to the mtpProperties
+os.system("mv train.cfg " + trainingConfigs)
+os.chdir(rootFolder)
+
+# Generate and run train job file (job file must be used to avoid clogging login nodes)
+trainJobTemplate = templatesFolder + "/trainMTP.qsub"
+trainJob = mtpFolder + "/trainMTP.qsub"
+shutil.copyfile(trainJobTemplate, trainJob)
+with open (trainJob, 'r+' ) as f:
+            content = f.read()
+            contentNew = re.sub("\$account", params["slurmParam"]["account"], content) 
+            contentNew = re.sub("\$partition", params["slurmParam"]["partition"], contentNew) 
+            contentNew = re.sub("\$qos", params["slurmParam"]["qos"], contentNew) 
+            contentNew = re.sub("\$mlp", params["mlpBinary"], contentNew)
+            contentNew = re.sub("\$mtp", mtpFile, contentNew)
+            contentNew = re.sub("\$train", trainingConfigs, contentNew)
+            f.seek(0)
+            f.write(contentNew)
+            f.truncate()
+
+exitCode = subprocess.Popen(["sbatch", trainJob]).wait()
+if(exitCode):
+    print("The mindist call has failed. Potential may be unstable. Exiting...")
+    quit()
+
+
+#endregion
 
 # except Exception as e:
 #     print("An error has occur during the generation of the initial files. Verify the formating of your JSON config file.")
